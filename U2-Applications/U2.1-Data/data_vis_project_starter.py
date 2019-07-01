03389864daba0db7b562e150afbfb26253eb735d
christinaliao11 <christinaliao11@gmail.com>
2019-07-01 14:51:10 -0700

    data project

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -13,8 +13,35 @@ tweetFile = open("TwitterData/tweets_small.json", "r")
 tweetData = json.load(tweetFile)
 tweetFile.close()
 
+tweettext = []
+tweetstring = ""
+for tweet in tweetData:
+    tweetstring += tweet["text"]
+
+
+
 # Continue your program below! 
 
-# Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+# print(tweetstring)
+tweetBlob = TextBlob(tweetstring)
+
+print(tweetBlob.translate(to="es"))
+
+blob_polarity = []
+for blob in tweettext:
+    blob_polarity.append(blob.polarity)
+
+blob_subjectivity = []
+for blob in tweettext:
+    blob_subjectivity.append(blob.blob_subjectivity)
+
+word_dict = {}
+
+for word in tweetBlob.words:
+    word_dict[word.lower()] = tweetBlob.word_counts[word.lower()]
+
+print(word_dict) 
+#blob_polarity.sort()
+#print(blob_polarity)
+
+tb = TextBlob("You are a horrible person")
--- U2-Applications/U2.1-Data/histogram_sample.py
+++ U2-Applications/U2.1-Data/histogram_sample.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
-someList = [0.2, -0.3, -0.4, -1, 1, 0.3, 0.6, 0.2, 0.14, -0.16, -0.18, 0.25]
-plt.hist(someList, bins=[-1, -0.5, 0.0, 0.5, 1])
+someList = [blob.polarity]
+plt.hist(someList, bins=[-1,-0.8, -0.5, 0.0, 0.5, 0.8, 1])
 plt.xlabel('Values')
 plt.ylabel('Number of Items')
 plt.title('Histogram of Numbers')
-plt.axis([-1.1, 1.1, 0, 6])
-plt.grid(True)
+plt.axis([-1.1,1.1,0,50])
+plt.grid(False)
 plt.show()
